3449fb2a7405c068dbbe78eef86bab69711d90dd
Adham-K-Fahmy <95886599+Adham-K-Fahmy@users.noreply.github.com>
2023-05-26 05:45:27 +0300

    Add files via upload

---
+++ StudentAffairsProject/urls.py
@@ -0,0 +1,28 @@
+
+from django.contrib import admin
+from django.urls import path, include
+from Students import views
+from django.conf import settings
+from django.conf.urls.static import static
+# from Students import search_view
+
+urlpatterns = [
+    path('add', views.AddStudentPage, name='AddStudentPage'),
+    path('add_student/', views.AddingSent, name='AddingSent'),
+    path('register/', views.RegisterPage, name='RegisterPage'),
+    path('register/sent/', views.RegisterSent, name='RegisterSent'),
+    path('list/', views.ListStudentPage, name='ListStudentPage'),
+    path('search', views.search_view, name='search'),
+    path('login/', views.login_page, name='loginpage'),
+    path('login/Login_validation', views.Login_validation, name='Login_validation'),
+    path('home/', views.HomePage, name='HomePage'),
+    path('', views.MainHomePage, name='MainHome'),
+    path('afterlogin/', views.AfterLoginPage, name='AfterLogin'),
+    path('StudentInfo/<int:student_id>/',
+         views.StudentInfo, name='StudentInfoPage'),
+    path('selectdepartment/<int:student_id>', views.SelectDepartment,
+         name='SelectDepartmentPage'),
+
+]
+
+urlpatterns += static(settings.STATIC_URL, document_root=settings.STATIC_ROOT)
